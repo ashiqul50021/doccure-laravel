1a9b664e7bac025970adda27916bcd705d1e3e46
ashiqul50021 <ashiqulislamrasel@gmail.com>
2026-02-05 03:36:33 +0600

    feat: add Doccure template styles for header and hero banner sections with new CSS and JS files.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -111,7 +111,7 @@ Version      : 1.0
 
 	$('body').append('<div class="sidebar-overlay"></div>');
 	$(document).on('click', '#mobile_btn', function () {
-		$('main-wrapper').toggleClass('slide-nav');
+		$('.main-wrapper').toggleClass('slide-nav');
 		$('.sidebar-overlay').toggleClass('opened');
 		$('html').addClass('menu-opened');
 		return false;
@@ -120,13 +120,13 @@ Version      : 1.0
 	$(document).on('click', '.sidebar-overlay', function () {
 		$('html').removeClass('menu-opened');
 		$(this).removeClass('opened');
-		$('main-wrapper').removeClass('slide-nav');
+		$('.main-wrapper').removeClass('slide-nav');
 	});
 
 	$(document).on('click', '#menu_close', function () {
 		$('html').removeClass('menu-opened');
 		$('.sidebar-overlay').removeClass('opened');
-		$('main-wrapper').removeClass('slide-nav');
+		$('.main-wrapper').removeClass('slide-nav');
 	});
 
 	// Mobile Menu
